486bd75d9432b8c71610115a940ab595c5167aab
Clover <68591353+justinangeloperez327@users.noreply.github.com>
2026-09-24 14:08:55 +0400

    feat: establish project manpower domain

---
+++ src/features/manpower/api/index.ts
@@ -0,0 +1,1 @@
+import { MockManpowerRepository } from "./manpower.mock.repository";export const manpowerRepository=new MockManpowerRepository();export type { ManpowerRepository } from "./manpower.repository";
