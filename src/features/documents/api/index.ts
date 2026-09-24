d480eabc19ae80d5850e9acd4883a8d619797b3e
Clover <68591353+justinangeloperez327@users.noreply.github.com>
2026-09-24 15:05:51 +0400

    feat: establish project document control domain

---
+++ src/features/documents/api/index.ts
@@ -0,0 +1,1 @@
+import { MockDocumentRepository } from "./document.mock.repository";export const documentRepository=new MockDocumentRepository();export type { DocumentRepository } from "./document.repository";
