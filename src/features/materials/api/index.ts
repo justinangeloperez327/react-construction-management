023e71f099f6e41bf66e17aae390e677fcfa08d3
Clover <68591353+justinangeloperez327@users.noreply.github.com>
2026-09-24 14:15:51 +0400

    feat: establish project materials domain

---
+++ src/features/materials/api/index.ts
@@ -0,0 +1,1 @@
+import { MockMaterialRepository } from "./material.mock.repository";export const materialRepository=new MockMaterialRepository();export type { MaterialRepository } from "./material.repository";
