e90cdb3c6b63881ad10e657908198183fdcee5bb
Clover <68591353+justinangeloperez327@users.noreply.github.com>
2026-09-24 13:55:48 +0400

    feat: establish project activities domain

---
+++ src/features/activities/api/index.ts
@@ -0,0 +1,1 @@
+import { MockActivityRepository } from "./activity.mock.repository";export const activityRepository=new MockActivityRepository();export type { ActivityRepository } from "./activity.repository";
