6d16589c200134e6377e91cd11f35c4a73664790
Clover <68591353+justinangeloperez327@users.noreply.github.com>
2026-09-24 15:55:12 +0400

    feat: establish project quality domain

---
+++ src/features/quality/api/index.ts
@@ -0,0 +1,1 @@
+import { MockQualityRepository } from "./quality.mock.repository";export const qualityRepository=new MockQualityRepository();export type { QualityRepository } from "./quality.repository";
